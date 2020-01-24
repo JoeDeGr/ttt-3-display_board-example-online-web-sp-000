d1ed5528d87ee7e6fb189f0483ac9cb9ce5a8271
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-01-24 02:39:57 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-def displayboard = "   |   |   
+def display_board 
+puts "   |   |   
   -----------
      |   |   
      -----------
         |   |   "
       end
-      displayboard
+      display_board
